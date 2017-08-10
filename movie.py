cb4a547b2696b07a9fdb349cf27faa1b669b90e9
Aaron Pang <aaron@192-58.priv23.nus.edu.sg>
2017-08-10 16:10:33 +0800

    conformed to PEP 8

--- movie.py
+++ movie.py
@@ -1,12 +1,10 @@
-"""
-This is a class that will store each of our movies.
-
-Each instance will have the name, description, poster image and youtube url.
-"""
-
-
 class Movie ():
     def __init__(self, title, description, poster, trailer):
+        """
+        This is a class that will store each of our movies.
+        Each instance will have the name, description,
+        poster image and youtube url.
+        """
         self.title = title
         self.description = description
         self.poster_image_url = poster
